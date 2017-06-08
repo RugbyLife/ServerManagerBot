d2b370c5ee7c359f40c6886192d835fa3e026dec
Leo Zbona <leonard.zbona@uoit.net>
2017-06-08 12:13:32 -0400

    Commit 6
    Starting the server info commad

---
+++ commando/random/serverinfo.js
@@ -0,0 +1,20 @@
+//This is a command to print out the server info
+
+const commando = require('discord.js-commando');
+
+class serverInfo extends commando.Command{
+
+  constructor(client){
+
+    super(client , {
+      name: serverinfo,
+      group: random,
+      memberName: serverinfo,
+      description: 'This command displays the number of people in the server'
+￼     });
+  }
+
+  async(message, args){
+
+  }
+}
