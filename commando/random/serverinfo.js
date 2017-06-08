54b7d982f91225e61185743f317ae9103d4481d2
Leo Zbona <leonard.zbona@uoit.net>
2017-06-08 14:05:41 -0400

    missing lines
    fixed some errors and syntax

--- commando/random/serverinfo.js
+++ commando/random/serverinfo.js
@@ -6,15 +6,15 @@ class serverInfo extends commando.Command{
 
   constructor(client){
 
-    super(client , {
+    super(client, {
       name: serverinfo,
       group: random,
       memberName: serverinfo,
       description: 'This command displays the number of people in the server'
 ￼     });
-  }
 
-  async(message, args){
+
+  async run(message, args){
 
   }
 }
